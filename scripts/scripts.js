678c1d7af71a37776053b5541b19e3588e117ad9
lhebia <lhebia@gmail.com>
2020-07-06 11:23:07 -0400

    Added mobile menu funcitonality and shifted styles to taste

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
-console.log('Hello, world');
+const lhApp = {};
+
+lhApp.init = () => {
+    console.log('Hello, world');
+}
+
+lhApp.mainMenuBtn = document.querySelector('.mainMenuBtn');
+lhApp.mainMenuBtn.addEventListener('click', () => {
+    const nav = document.querySelector('.mainMenuNav');
+    nav.classList.toggle('makeVisible');
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    lhApp.init();
+});
